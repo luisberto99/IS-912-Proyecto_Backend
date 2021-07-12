090c0741818abc9558adf2029872045e16b40426
Alberto Medina <luisitomedina95@gmail.com>
2021-07-12 00:02:42 -0600

    Update estructurasJSONs.js

---
+++ estructurasJSONs.js
@@ -0,0 +1,64 @@
+categoriaEmpresas = [{
+    id: 1,
+    nombreCategoria: "",
+    empresas: [{
+        nombreComericalEmpresa: "",
+        RTN: "",
+        direccion: "",
+        productos: [{
+            nombreProducto: "",
+            precio: ""
+        }]
+    }]
+}];
+
+categoriasProductos = [{
+    id: 1,
+    nombreCategoria: "",
+    productos: [{
+        nombreProducto: "",
+        precio: ""
+    }]
+}];
+
+usuarios = [{
+    id: "lkdjsflakssjdlkfjal",
+    nombreUsurio: "",
+    ordenes: [{
+        id: "alksdfjlkasdfkjaldf",
+        idOrden: "123456789"
+    }]
+}];
+
+motoristas = [{
+    id: "klajdflkasdf",
+    nombreMotorista: "",
+    estado: true,
+    verificado: {
+        userVerificador: "",
+        fecha: ""
+    },
+    ordenesEntregadas: [{
+        id: "ajsdfljakslfj",
+        idOrden: "123456789"
+    }],
+    //ORDENES TOMADAS PENDIENTES DE ENTREGAR
+    ordenesTomadas: [{
+        id: "ajsdfljakslfj",
+        idOrden: "123456789"
+    }]
+}];
+
+ordenes = [{
+    id: "123456789",
+    fecha: "",
+    direccion: "",
+    motorista: "",
+    direccion: "",
+    coordenadas: "",
+    productos: [{
+        id: "laksjdflakjsdf", //id orden
+        cantidad: "",
+        comentarios: ""
+    }]
+}];
