7c1ad071cb8b2376ca7f72e7be523d67c086ac90
Luis Banegas <55450718+Shadow504@users.noreply.github.com>
2021-08-13 02:08:56 -0600

    APIs configuracion cuenta motorista,login, verificacion datos y encriptacion de datos.

--- routers/motoristas-router.js
+++ routers/motoristas-router.js
@@ -4,7 +4,7 @@ const motoristas = require('../models/motoristas');
 const mongoose = require('mongoose');
 var CryptoJS = require("crypto-js");
 
-
+/* APIS MOTORISTAS */
 /* OBTENER TODOS LOS MOTORISTAS VERIFICADOS */
 router.get('/verificados',(req,res)=>{
     
@@ -18,7 +18,6 @@ router.get('/verificados',(req,res)=>{
    }) 
 });
 
-module.exports = router;
 
 /* OBTENER LOS MOTORISTAS AUN FALTANTES POR VERIFICAR */
 
@@ -37,37 +36,44 @@ router.get('/faltanVerificar',(req,res)=>{
 /* CREAR NUEVO MOTORISTA */
 
 
-
 router.post('/registrar',(req,res)=>{
 
-    motoristas.find({email:req.body.email},{email:true}).then(result =>{
-        console.log(result.result.length);
-        if(result.length == 0){
+    
+    motoristas.find({},{email:true}).then(result =>{
+        let emails = [];
 
-            if(!emailExiste){
-                motoristas.create({
-                    primerNombre :req.body.primerNombre,
-                    primerApellido :req.body.primerApellido,
-                    numeroIdentidad :req.body.numeroIdentidad,
-                    numeroTelefono :req.body.numeroTelefono,
-                    email :req.body.email,
-                    password :req.body.password,
-                    domicilio :req.body.domicilio,
-                    estadoVerificacionMotorista :false ,
-                    IdAdministradoVerificoMotorista: "",
-                    ordenesEntregadas :[] ,
-                    ordenTomadaActualmente :[]
-                }).then(result =>{
-                    res.send({result:true});
-                    res.end();
-                }).catch(e =>{
-                    res.send({result:false});
-                    res.end();
-                })
-            }else{
+        result.forEach(user => {
+            // console.log(user);
+            let bytes  = CryptoJS.AES.decrypt(user.email, 'secretKeyDW2021***');
+            let emailDescript = bytes.toString(CryptoJS.enc.Utf8);
+
+            emails.push(emailDescript);
+        });
+        // console.log(emails);
+
+        if(!(emails.includes(req.body.email))){
+            let emailCrypto = CryptoJS.AES.encrypt(req.body.email, 'secretKeyDW2021***').toString();
+            let passCrypto = CryptoJS.AES.encrypt(req.body.password, 'secretKeyDW2021***').toString();
+
+            motoristas.create({
+                primerNombre :req.body.primerNombre,
+                primerApellido :req.body.primerApellido,
+                numeroIdentidad :req.body.numeroIdentidad,
+                numeroTelefono :req.body.numeroTelefono,
+                email :emailCrypto,
+                password :passCrypto,
+                domicilio :req.body.domicilio,
+                estadoVerificacionMotorista :false ,
+                ordenesEntregadas :[] ,
+                ordenTomadaActualmente :[]
+            }).then(result =>{
+                res.send({result:true});
+                res.end();
+            }).catch(e =>{
                 res.send({result:false});
                 res.end();
-            }
+            })
+            
         }else{
           res.send({result:false});
           res.end();
@@ -78,31 +84,59 @@ router.post('/registrar',(req,res)=>{
 });
 
 
-
 /* CAMBIAR ESTADO DEL MOTORISTA A VERIFICADO, DESDE ADMINISTRACION */
 
 router.put('/:idMotorista/verificar',(req,res)=>{
     
-    motoristas.updateOne({_id:req.params.idMotorista},{"RegistroVerificacionCuenta.nombre":req.body.nombreAdmin, "RegistroVerificacionCuenta.apellido":req.body.apellidoAdmin, "RegistroVerificacionCuenta._idAdmin":mongoose.Types.ObjectId(req.body.idAdmin),estadoVerificacionMotorista:true}).then(result =>{
+    motoristas.updateOne({_id:req.params.idMotorista},{$push:{ RegistroVerificacionCuenta: {nombre:req.body.nombreAdmin,apellido:req.body.apellidoAdmin,_idAdministrador: mongoose.Types.ObjectId(req.body.idAdmin)}},estadoVerificacionMotorista:true}).then(result =>{
         res.send(result);
         res.end();
     }).catch(e =>{
-        res.send(e);
+        res.send({result:false});
         res.end();
     });
 });
 
 
-/* OBTENER CREDENCIALES PARA VERIFICAR REGISTRO DEL MOTORISTA */
-router.get('/obtenerDatos/:email',(req,res)=>{
-    motoristas.findOne({email:req.params.email}).then(result =>{
-        res.send({result:result.password, idUser: result._id});
-        res.end();
 
-    }).catch(e =>{
-        res.send({result:false});
+/* API PARA VERIFICAR SI LA CUENTA DE UN MOTORISTA EXISTE. */
+
+router.get('/obtenerDatos/:email',(req,res)=>{
+
+    motoristas.find({},{email:true}).then(result =>{
+        let emails = [];
+
+        result.forEach(user => {
+            // console.log(user);
+            let bytes  = CryptoJS.AES.decrypt(user.email, 'secretKeyDW2021***');
+            let emailDescript = bytes.toString(CryptoJS.enc.Utf8);
+
+            emails.push(emailDescript);
+        });
+
+        if(emails.includes(req.params.email)){
+            res.send(true);
+            res.end();
+            
+        }else{
+            res.send(false);
+            res.end();
+        }
+        
+    }).catch(e=>{
+        res.send(false);
         res.end();
     })
+    
+    // motoristas.findOne({email:req.params.email}).then(result =>{
+    //     //     res.send({result:result.password, idUser: result._id});
+    //     //     res.end();
+    
+    //     // }).catch(e =>{
+    //     //     res.send({result:false});
+    //     //     res.end();
+    // })
+
 });
 
 /* AGREGAR IMAGEN A PERFIL MOTORISTA */
@@ -120,10 +154,75 @@ router.post('/newImage',(req,res)=>{
 
 });
 
+/* API PARA OBTENER IMAGEN DEL PERFIL DEL MOTORISTA */
+
+router.get('/obtenerImagenPerfil/:idUser',(req,res)=>{
+
+    motoristas.findOne({_id:req.params.idUser},{imagenPerfil:true}).then(result =>{
+        res.send(result.imagenPerfil);
+        res.end();
+
+    }).catch(e =>{
+        res.send(false);
+        res.end();
+    })
+
+});
+
+/* LOGIN MOTORISTA */
+
+router.post('/login',(req,res)=>{
+
+    motoristas.find({},{email:true,password:true}).then(result =>{
+        let verify = true;
+        result.forEach(user => {
+            // console.log(user);
+
+            let bytes  = CryptoJS.AES.decrypt(user.email, 'secretKeyDW2021***');
+            let emailDecrypt = bytes.toString(CryptoJS.enc.Utf8);
+
+            let bytesPass  = CryptoJS.AES.decrypt(user.password, 'secretKeyDW2021***');
+            let passDecrypt = bytesPass.toString(CryptoJS.enc.Utf8);
+            // console.log(emailDecrypt,passDecrypt);
+            if(req.body.password == passDecrypt && req.body.email == emailDecrypt){
+              res.send({result:true, id:user._id});
+              res.end();  
+              verify = false
+            }
+
+        });
+        if(verify){
+            res.send({result:false});
+            res.end();  
+        }
+        
+    }).catch(e=>{
+        res.send(false);
+        res.end();
+    })
+
+});
 
 
+/* CONFIGURACION PERFIL USUARIO, ACTUALIZAR CORREO MOTORISTA */
 
-/* ACTUALIZAR CORREO MOTORISTA */
+router.put("/configuracion/cambiarCorreo",(req,res)=>{
+
+    let emailEncrypt = CryptoJS.AES.encrypt(req.body.email, 'secretKeyDW2021***').toString();
+
+    motoristas.updateOne({_id:req.body.idUser},{email:emailEncrypt}).then(result =>{
+
+        res.send(result);
+        res.end();
+    }).catch(e =>{
+        res.send({result:false});
+        res.end();
+    });
+
+});
+
+
 /* ACTUALIZAR PASSWORD MOTORISTA */
 /* OBTENER MOTORISTAS DISPONIBLES */
 
+module.exports = router;
