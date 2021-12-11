3c6c534e2ce71ee4e0b57912272073311e4cd1d9
Alberto Medina <luisitomedina95@gmail.com>
2021-12-11 16:52:05 -0600

    Clientes consultas asincrónicas

--- routers/clientes-router.js
+++ routers/clientes-router.js
@@ -1,27 +1,31 @@
 var express = require('express');
 var router = express.Router();
-var clientes = require('../models/clientes');
 var mongoose = require('mongoose');
+const data = require('../modules/database');
 
 
 //Obtener clientes
-router.get('/', function(req, res) {
-    clientes.find({}, {
-            password: false
-        })
-        .then(result => {
-            res.send(result);
-            res.end();
-        })
-        .catch(error => {
-            console.log('error');
-            res.send(error);
-            res.end();
-        });
+router.get('/', async function(req, res) {
+    /* CONECCION A LA BASE DE DATOS */
+    const db = await data.connectToDatabase();
+    /* OBTENER COLLECION CLIENTES */
+    const clientes = await db.collection('clientes');
+    /* CONSULTA */
+    result = await clientes.find({}, {
+        password: false
+    }).toArray();
+
+    res.send(result);
+    res.end();
 });
 
 /* AGREGAR UN NUEVO CLIENTE */
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
+    /* CONECCION A LA BASE DE DATOS */
+    const db = await data.connectToDatabase();
+    /* OBTENER COLLECION CLIENTES */
+    const clientes = await db.collection('clientes');
+    /* CONSULTA */
     let u = new clientes({
         nombre: req.body.nombre,
         apellido: req.body.apellido,
@@ -35,91 +39,89 @@ router.post('/', (req, res) => {
         ordenes: [],
         estado: "activo"
     });
-    u.save().then(result => {
-            res.send(result);
-            res.end();
-        })
-        .catch(error => {
-            res.send(error);
-            res.end();
-        });
+
+    result = await clientes.insertOne(u);
+    res.send(result);
+    res.end();
 });
 
 //Obtener un cliente
-router.get('/:idCliente', function(req, res) {
-    clientes.find({
-            _id: mongoose.Types.ObjectId(req.params.idCliente)
-        }, {
-            password: false
-        })
-        .then(result => {
-            res.send(result[0]);
-            res.end();
-        })
-        .catch(error => {
-            console.log('error');
-            res.send(error);
-            res.end();
-        });
+router.get('/:idCliente', async function(req, res) {
+    /* CONECCION A LA BASE DE DATOS */
+    const db = await data.connectToDatabase();
+    /* OBTENER COLLECION CLIENTES */
+    const clientes = await db.collection('clientes');
+    /* CONSULTA */
+    result = await clientes.find({
+        _id: mongoose.Types.ObjectId(req.params.idCliente)
+    }, {
+        password: false
+    }).toArray();
+
+    res.send(result[0]);
+    res.end();
 });
+
+
 //Obtener un cliente con correo
-router.get('/:email/email', function(req, res) {
-    console.log(typeof(req.params.email))
+router.get('/:email/email', async function(req, res) {
+    /* CONECCION A LA BASE DE DATOS */
+    const db = await data.connectToDatabase();
+    /* OBTENER COLLECION CLIENTES */
+    const clientes = await db.collection('clientes');
+    /* CONSULTA */
+    result = await console.log(typeof(req.params.email))
     clientes.find({
-            correoElectronico: req.params.email
-        }, {
-            password: true,
-            _id: true,
-            correoElectronico: true
-        })
-        .then(result => {
-            res.send(result[0]);
-            res.end();
-        })
-        .catch(error => {
-            console.log('error');
-            res.send(error);
-            res.end();
-        });
+        correoElectronico: req.params.email
+    }, {
+        password: true,
+        _id: true,
+        correoElectronico: true
+    }).toArray();
+
+    res.send(result[0]);
+    res.end();
 });
 
 /* MODIFICAR UN CLIENTE */
-router.put('/:idCliente', (req, res) => {
-    clientes.updateOne({
-            _id: mongoose.Types.ObjectId(req.params.idCliente)
-        }, {
-            nombre: req.body.nombre,
-            apellido: req.body.apellido,
-            imagenPerfil: req.body.imagenPerfil,
-            correoElectronico: req.body.correoElectronico,
-            fechaRegistro: new Date,
-            password: req.body.password,
-            telefono: req.body.telefono,
-            ubicacion: req.body.ubicacion,
-        }).then(result => {
-            res.send(result);
-            res.end();
-        })
-        .catch(error => {
-            res.send(error);
-            res.end();
-        });
+router.put('/:idCliente', async(req, res) => {
+    /* CONECCION A LA BASE DE DATOS */
+    const db = await data.connectToDatabase();
+    /* OBTENER COLLECION CLIENTES */
+    const clientes = await db.collection('clientes');
+    /* CONSULTA */
+    result = await clientes.updateOne({
+        _id: mongoose.Types.ObjectId(req.params.idCliente)
+    }, {
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        imagenPerfil: req.body.imagenPerfil,
+        correoElectronico: req.body.correoElectronico,
+        fechaRegistro: new Date,
+        password: req.body.password,
+        telefono: req.body.telefono,
+        ubicacion: req.body.ubicacion,
+    });
+
+    res.send(result);
+    res.end();
 });
 
 /* ELIMINAR UN CLIENTE - ESTADO INACTIVO */
-router.delete('/:idCliente', (req, res) => {
-    clientes.updateOne({
-            _id: mongoose.Types.ObjectId(req.params.idCliente)
-        }, {
-            estado: "INACTIVO"
-        }).then(result => {
-            res.send(result);
-            res.end();
-        })
-        .catch(error => {
-            res.send(error);
-            res.end();
-        });
+router.delete('/:idCliente', async(req, res) => {
+    /* CONECCION A LA BASE DE DATOS */
+    const db = await data.connectToDatabase();
+    /* OBTENER COLLECION CLIENTES */
+    const clientes = await db.collection('clientes');
+    /* CONSULTA */
+    result = await clientes.updateOne({
+        _id: mongoose.Types.ObjectId(req.params.idCliente)
+    }, {
+        estado: "INACTIVO"
+    });
+
+    res.send(result);
+    res.end();
 });
 
 
